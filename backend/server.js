f319b2b2c2c12d58733b21add2f493b8d44bda8f
thefaizwork <workfaizan.ahmad@gmail.com>
2024-07-25 02:06:39 +0530

    auth route setup

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
+//imprt routes
+import authRoutes from "./routes/auth.route.js"
+app.use("/api/auth", authRoutes)
+
+
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT )
 })
